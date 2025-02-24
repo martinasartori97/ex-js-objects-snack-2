b68aaaa7f422c041c9369cf07200122cb3de4050
martinasartori97 <martinasartori9@gmail.com>
2025-02-24 15:23:45 +0100

    initial commit

---
+++ codeQuestion.js
@@ -0,0 +1,20 @@
+// CODE QUESTION 1:
+
+// - in console viene stampato 'Double Cheese Burger'
+//     - è stato creato un solo oggetto
+
+// CODE QUESTION 2:
+
+// - in console stamperà 'salad'
+//     - sono stati creati 3 oggetti
+
+// CODE QUESTION 3:
+
+// -
+
+//     CODE QUESTION 4:
+
+// - Il metodo migliore per clonare l'oggetto chef in questo caso è lo spread operator
+// perchè non ha oggetti annidati profondamente
+
+//     - per l'oggetto restaurant invece userei una clonazione profonda essendoci anche oggetti annidati
